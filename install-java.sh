faa56a2ca1aca7f8a4626cbe0cbc081cc330638a
Paul Orion Weaver <awesomeboy7865@gmail.com>
2025-03-04 19:30:01 -0700

    Create install-java.sh
    Script checks for 3 conditions after installing java

---
+++ install-java.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+apt update
+apt install -y default-jre
+
+
+install_result=$(java -version 2>&1 > /dev/null | grep "java version\|openjdk version" | awk '{print substr($3,2,2)}')
+
+if [ "$install_result" == "" ]
+then
+ echo "No java version found"
+elif [ "$install_result" == "1." ]
+then
+ echo "An old version of Java has been found."
+elif [ "$install_result" -ge 11 ]
+then
+ echo "Java version 11 or greater is installed successfully."
+fi
