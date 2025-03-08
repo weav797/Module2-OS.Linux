c2f4ee71b748d47b5eed96397481332de2908b5f
Paul Orion Weaver <awesomeboy7865@gmail.com>
2025-03-07 17:41:14 -0700

    Update user-processes.sh

--- user-processes.sh
+++ user-processes.sh
@@ -8,7 +8,7 @@
 # Write a bash script using Vim editor that checks all the processes running for the current user (USER env var) and prints out the processes in console.
 # 
 # EXERCISE 4: Bash Script - User Processes Sorted
-#Extend the previous script to ask for a user input for sorting the processes output either by memory or CPU consumption, and print the sorted list.
+# Extend the previous script to ask for a user input for sorting the processes output either by memory or CPU consumption, and print the sorted list.
 # 
 # EXERCISE 5: Bash Script - Number of User Processes Sorted
 # Extend the previous script to ask additionally for user input about how many processes to print.
